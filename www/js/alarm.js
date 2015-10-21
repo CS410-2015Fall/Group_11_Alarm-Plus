1f72c470e3e267262f73b5f04c63959a660be712
Aor_N <scc385@gmail.com>
2015-10-20 19:32:16 -0700

    make an alarm independent

--- www/js/alarm.js
+++ www/js/alarm.js
@@ -5,26 +5,44 @@ Alarm = function(name, hour, minute, timeofday, task) {
     this.tod = timeofday;
     this.task = task;
     this.snoozeCredit = 3;
-    this.active = true;
+    this.active;
 };
 
-// Alarm inherits from Clock
-Alarm.prototype = new Clock();
+Alarm.prototype.start = function() {
+    var today = new Date();
+    var currentHours = today.getHours();
+    var currentMinutes = today.getMinutes();
+    var currentSeconds = today.getSeconds();
 
-Alarm.prototype.disableAlarm = function() {
-  this.active = false;
+    // Pad the minutes and seconds with leading zeros, if required
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+    // set the format of the time
+    var timeOfDay = (currentHours < 12) ? "AM" : "PM";
+    currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+    currentHours = (currentHours === 0) ? 12 : currentHours;
+
+    if (this.hour == currentHours && this.minute == currentMinutes && this.tod === timeOfDay) {
+        // if Time is up do something here:
+        console.log("TIME IS UP");
+        return;
+    }
+    active = setTimeout(this.start(), 60000);
 };
 
-Alarm.prototype.enableAlarm = function() {
-  this.active = true;
+Alarm.prototype.stop = function() {
+    clearTimeout(this.active);
 };
 
-Alarm.prototype.isEnable = function() {
-  return this.active;
+Alarm.prototype.setHour = function(hour) {
+    this.hour = hour;
 };
 
-Alarm.prototype.snooze = function() {
-
+Alarm.prototype.setMinute = function(minute) {
+    this.minute = minute;
 };
 
-
+Alarm.prototype.setTimeOfDay = function(tod) {
+    this.tod = tod;
+};
--- www/js/main.js
+++ www/js/main.js
@@ -10,64 +10,32 @@ var checkInput = function checkNumberInput(object) {
     }
 };
 
-// singleton object to be used:
-var clock = new Clock();
-var alarms = []; // hashtable
-// var alarms = new Hashtable();
-var startAlarm;
-var startTask;
-var snooze = 3;
-
 // Start the clock
 jq("#tempt-clock-run").click(function() {
     // we will wait for the second to hit 0 before we start running the clock.
-    setTimeout(function() {
-        startAlarm = setInterval(function() {
-            console.log(clock.dispTime());
-            isAlarmTime(alarms);
-        }, 60000);
-    }, (60 - clock.getSecond()) * 1000);
+
 });
 
-// Stop the clock
-jq("#tempt-clock-stop").click(function() {
-    clearInterval(startAlarm);
-});
-
+// alarms will contain all the created alarms.
+var alarms = [];
 
 jq("#create-alarm").click(function() {
     // Check if the inputs are valid:
 
     // Create an alarm based on user's input
     var id = Math.floor((Math.random() * 15) + 1);
-    var alarm = new Alarm(jq("#alarm-name").val(), jq("#alarm-hour").val(),
+    alarms[id] = new Alarm(jq("#alarm-name").val(), jq("#alarm-hour").val(),
         jq("#alarm-minute").val(), jq("#time-of-day").val());
-    alarms[id] = alarm;
+
+    // start an alarm at second = 0
+    setTimeout(function() {
+      alarms[id].start();
+    }, (60 - clock.getSecond()) * 1000);
+
     console.log(alarms);
     ClearInputBox();
 });
 
-// Constantly check if an alarm should go off.
-// Alarms = the list of alarms
-function isAlarmTime(Alarms) {
-    for (var id in alarms) {
-        if (!Alarms[id].active) {
-            continue;
-        }
-        snooze = 3; // snooze should be set to 3 at the start
-        var aName = Alarms[id].name;
-        var aHour = Alarms[id].hour;
-        var aMinute = Alarms[id].minute;
-        var aTod = Alarms[id].tod;
-        var aActive = Alarms[id].active;
-        var aSnooze = Alarms[id].snoozeCredit;
-        //console.log("alarm name is " + aName + " and my alarm hour is " + aHour + " and alarm minute is " + aMinute + " " + aTod + " " + aActive);
-        if (aHour == clock.getHour() && aMinute == clock.getMinute() && aTod === clock.getTimeOfDay()) {
-            // keep rining:
-            alert('wake up already');
-        }
-    }
-}
 
 jq("#tempt-snooze").click(function() {
     snooze--;
@@ -86,4 +54,3 @@ function ClearInputBox() {
 jq("#tempt-check-time").click(function() {
     isAlarmTime(alarms);
 });
-
