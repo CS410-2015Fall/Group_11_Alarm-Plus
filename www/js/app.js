65efff02ea1b09e430e4f7548acbc0f4466c0e2a
Aor_N <scc385@gmail.com>
2015-10-21 22:45:09 -0700

    Successfully sync the ngCordova and the controller.js

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var AlarmPlus = angular.module('Alarm-Plus', ['ionic']);
+angular.module('Alarm-Plus', ['ionic', 'Alarm-Plus.controllers', 'ngCordova'])
 
-AlarmPlus.run(function($ionicPlatform) {
+.run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
@@ -16,16 +16,4 @@ AlarmPlus.run(function($ionicPlatform) {
             StatusBar.styleDefault();
         }
     });
-});
-
-AlarmPlus.controller('setupController', ['$scope', listAlarms]);
-
-function listAlarms($scope) {
-  $scope.defaultMinute = 0,
-  $scope.defaultHour = 0,
-  $scope.defaultName ="CoolAlarm",
-  $scope.defaultTod = "PM";
-  $scope.tod = ["AM", "PM"];
-}
-
-
+})
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,11 @@
-/*angular.module('starter', ['ionic', 'ngCordova'])
+angular.module('Alarm-Plus.controllers', ['ionic', 'ngCordova'])
 
-.controller('setupController', ['$scope', listAlarms]);
-
-function listAlarms($scope, $ionicPlatform) {
-
-    $scope.defaultMinute = 0,
-        $scope.defaultHour = 0,
-        $scope.defaultName = "CoolAlarm",
-        $scope.defaultTod = "PM";
-    $scope.tod = ["AM", "PM"];
-}
-*/
+.controller('setupController', function($scope, $ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        $scope.defaultMinute = 0,
+            $scope.defaultHour = 0,
+            $scope.defaultName = "CoolAlarm",
+            $scope.defaultTod = "PM";
+        $scope.tod = ["AM", "PM"];
+    })
+});
