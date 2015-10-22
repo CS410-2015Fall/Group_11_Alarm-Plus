7610c4215b47fa0bd5fa80522f8a879a52868f85
Aor_N <scc385@gmail.com>
2015-10-21 22:17:40 -0700

    add js just for controllers

---
+++ www/js/controllers.js
@@ -0,0 +1,13 @@
+/*angular.module('starter', ['ionic', 'ngCordova'])
+
+.controller('setupController', ['$scope', listAlarms]);
+
+function listAlarms($scope, $ionicPlatform) {
+
+    $scope.defaultMinute = 0,
+        $scope.defaultHour = 0,
+        $scope.defaultName = "CoolAlarm",
+        $scope.defaultTod = "PM";
+    $scope.tod = ["AM", "PM"];
+}
+*/
