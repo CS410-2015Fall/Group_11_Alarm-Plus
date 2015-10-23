f3cd2dba6e7195b3bbe9efbe1e673f2e152b6d3d
Aor_N <scc385@gmail.com>
2015-10-23 14:30:23 -0700

    add cool stuff to test task

--- www/js/task/task.controllers.js
+++ www/js/task/task.controllers.js
@@ -2,7 +2,14 @@ angular.module('Alarm-Plus.controllers')
 
 .controller('taskController', function($scope, $ionicPlatform) {
     $ionicPlatform.ready(function() {
+      $scope.taskName = "hi there";
+      $scope.fixedText = "im fixed";
 
+      $scope.curPoint = 0;
+
+      $scope.success = function () {
+        $scope.curPoint++;
+      }
 
     });
 });
