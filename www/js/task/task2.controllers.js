b6dd9b549fb6809643910b8eeae3ca9199f1f188
Aor_N <scc385@gmail.com>
2015-11-06 14:24:09 -0800

    sync the task2

---
+++ www/js/task/task2.controllers.js
@@ -0,0 +1,9 @@
+angular.module('Alarm-Plus.controllers')
+
+// newGameController
+.controller('task2Controller', ['$scope', function($scope) {
+
+
+  $scope.hi = "okokok";
+  $scope.setup = "task2"
+}]);
