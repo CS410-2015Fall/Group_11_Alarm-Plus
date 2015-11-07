e0fc880f053abf45e37a7dfcc10b4afa3199b059
Aor_N <scc385@gmail.com>
2015-11-07 15:14:31 -0800

    remove unneccesary codes

--- www/js/setup/alarm.js
+++ www/js/setup/alarm.js
@@ -1,6 +1,5 @@
 angular.module('Alarm-Plus.controllers')
 
-
 .factory('Alarm', ['$ionicPopup', '$timeout', '$state', function($ionicPopup, $timeout, $state) {
     function Alarm( name, hour, minute, timeofday, weekDays, task) {
         this.name = name;
@@ -11,56 +10,6 @@ angular.module('Alarm-Plus.controllers')
         this.snoozeCredit = 3;
         this.active = {};
         this.weekDays = weekDays; // TODO: weekdays should be an array of number
-    };
-
-    function showPopup($scope) {
-        var myPopup = $ionicPopup.show({
-            template: '<input type="password">',
-            title: 'Enter Wi-Fi Password',
-            subTitle: 'Please use normal things',
-            buttons: [{
-                text: 'Cancel'
-            }, {
-                text: '<b>Save</b>',
-                type: 'button-positive'
-            }]
-        });
-        $timeout(function() {
-            myPopup.close(); //close the popup after 3 seconds for some reason
-        }, 10000);
-    }
-
-    Alarm.prototype.start = function() {
-        var today = new Date();
-        var day = today.getDay();
-        var currentHours = today.getHours();
-        var currentMinutes = today.getMinutes();
-        var currentSeconds = today.getSeconds();
-
-        // Pad the minutes and seconds with leading zeros, if required
-        currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
-        currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
-
-        // set the format of the time
-        var timeOfDay = (currentHours < 12) ? "AM" : "PM";
-        currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
-        currentHours = (currentHours === 0) ? 12 : currentHours;
-        var curTime = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-        if (this.hour == currentHours && this.minute == currentMinutes && this.tod === timeOfDay &&
-            this.weekDays[day].checked) {
-            console.log("TIME IS UP");
-            $state.go('app.task');
-            // With this setup: music and everything should be done in Task page.
-        }
-
-        this.active = setTimeout((function() {
-            this.start();
-        }).bind(this), 60000);
-    };
-
-    // Stop the Alarm from running
-    Alarm.prototype.stop = function() {
-        clearInterval(active);
     };
 
     Alarm.prototype.setHour = function(hour) {
@@ -75,10 +24,5 @@ angular.module('Alarm-Plus.controllers')
         this.tod = tod;
     };
 
-    // When the user press snooze button, lower the snooze credit
-    Alarm.prototype.useSnooze = function() {
-        this.snoozeCredit--;
-        console.log(this.snoozeCredit);
-    };
     return Alarm;
 }])
