f8df5f85854a8960a3e9c342d43abbabf566288c
ivitaminb <ivitaminb@gmail.com>
2015-11-13 17:30:04 -0800

    Added 5 questions to the taskHistory branch. Working task2controllers and task2.html.

--- www/js/task/task2.controllers.js
+++ www/js/task/task2.controllers.js
@@ -1,9 +1,97 @@
 angular.module('Alarm-Plus.controllers')
 
 // newGameController
-.controller('task2Controller', ['$scope', function($scope) {
+.controller('task2Controller', function($scope, $ionicSwipeCardDelegate, $cordovaVibration, $state, $rootScope) {
+            $scope.cards = [{
+                            title: 'Swipe down to clear the card',
+                            histq: 'Which year the Second World War end?',
+                            answer: 1945, //answer
+                            choices: [1945, 1939, 1918, 1914],
+                            rank: 0.5 - Math.random()
+                            }, {
+                            title: 'Swipe down to clear the card',
+                            histq: 'How many provinces are there in Canada?',
+                            answer: 10, //answer
+                            choices: [50, 10, 9, 11],
+                            rank: 0.5 - Math.random()
+                            }, {
+                            title: 'Swipe down to clear the card',
+                            histq: 'Who was the first Prime Minister of Canad?a',
+                            answer: "John A. Macdonald", //answer
+                            choices: ["John A. Macdonald", "Pierre Trudeau", "Paul Martin", "Brian Mulroney"],
+                            rank: 0.5 - Math.random()
+                            }, {
+                            title: 'Swipe down to clear the card',
+                            histq: 'Who designed the current flag of Canada?',
+                            answer: "George Stanley", //answer
+                            choices: ["King George V", "William Lyon Mackenzie King", "George Stanley", "John Cabot"],
+                            rank: 0.5 - Math.random()
+                            }, {
+                            title: 'Swipe down to clear the card',
+                            histq: 'Which is the largest Great Lake?',
+                            answer: "Lake Superior", //answer
+                            choices: ["Lake Erie", "Lake Huron", "Lake Ontario", "Lake Superior"],
+                            rank: 0.5 - Math.random()
+                            }];
+            
+            $scope.count = 3;
+            $scope.currIndex = "";
+            
+          
+            
+            $scope.randomQ = function() {
+            return Math.floor((Math.random() * 5) + 1);
+            }
+            
+            $scope.setSelected = function(selected) {
+            $scope.currIndex = selected;
+            };
+            
+            $scope.cardDestroyed = function(index) {
+            console.log("destroy " + index);
+            $scope.cards.splice(index, 1);
+            };
+            
+//            $scope.cardSwiped = function(index) {
+//            console.log("swipe " + index);
+//            var newCard = {
+//            title: 'Swipe down to clear the card',
+//            equation: '8 - x = 2',
+//            answer: 6, //answer
+//            choices: [6, 7, 9, 10]
+//            };
+//            $scope.cards.push(newCard);
+//            };
+            
+            $scope.self = $scope;
+            $scope.addCard = function() {
+            var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+            // newCard.id = Math.random();
+            //$scope.cards.push(angular.extend({}, newCard));
+            }
+            
+           
+            
+            $scope.random = function() {
+            return 0.5 - Math.random();
+            };
+            
+            $scope.isCorrect = function(answer, index) {
+            console.log(answer);
+            if (answer == $scope.cards[index].answer) {
+            console.log("count is " + $scope.count);
+            $scope.count = $scope.count - 1;
+            var card = $ionicSwipeCardDelegate.getSwipeableCard(this);
+            card.swipe();
+            } else {
+            //$cordovaVibration.vibrate(100);
+            }
+            
+            if ($scope.count == 0) {
+            $state.go('app.home');
+            }
+            
+            };
+            })
 
-
-  $scope.hi = "okokok";
-  $scope.setup = "task2"
-}]);
+//$state.go('app.home');
