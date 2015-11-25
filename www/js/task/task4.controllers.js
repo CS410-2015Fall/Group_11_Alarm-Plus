32849f688c8a08028cc06061af41fecbe8e3a39a
Bryan Lee <ivitaminb@gmail.com>
2015-11-25 15:53:22 -0800

    added 10 hard math questions
    10 questions added into task4.controllers.js

--- www/js/task/task4.controllers.js
+++ www/js/task/task4.controllers.js
@@ -16,51 +16,51 @@ angular.module('Alarm-Plus.controllers')
                             rank: 0.5 - Math.random()
                             }, {
                             title: 'Swipe down to clear the card',
-                            hardm: 'Who was the first Prime Minister of Canada?',
-                            answer: "John A. Macdonald", //answer
-                            choices: ["John A. Macdonald", "Pierre Trudeau", "Paul Martin", "Brian Mulroney"],
+                            hardm: 'If I flip a coin 4 times, what is the probability that I get 4 heads?',
+                            answer: "0.0625", //answer
+                            choices: ["0/025", "1", "0.25", "0.0625"],
                             rank: 0.5 - Math.random()
                             }, {
                             title: 'Swipe down to clear the card',
-                            hardm: 'Who designed the current flag of Canada?',
-                            answer: "George Stanley", //answer
-                            choices: ["King George V", "William Lyon Mackenzie King", "George Stanley", "John Cabot"],
+                            hardm: '2 + 2 x 2 - 2 = ?',
+                            answer: "4", //answer
+                            choices: ["2", "4", "6", "8"],
                             rank: 0.5 - Math.random()
                             }, {
                             title: 'Swipe down to clear the card',
-                            hardm: 'Which is the largest Great Lake?',
-                            answer: "Lake Superior", //answer
-                            choices: ["Lake Erie", "Lake Huron", "Lake Ontario", "Lake Superior"],
+                            hardm: '5 x 5 + 5 - 5 + 5 = ?',
+                            answer: "30", //answer
+                            choices: ["25", "40", "30", "50"],
                             rank: 0.5 - Math.random()
                             }, {
                             title: 'Swipe down to clear the card',
-                            hardm: 'How many teams were there when the NHL was founded?',
+                            hardm: '6 x 6 + 6 - 6 x 6 = ?',
                             answer: "6", //answer
-                            choices: ["5", "6", "7", "12"],
+                            choices: ["0", "6", "12", "36"],
                             rank: 0.5 - Math.random()
                             }, {
                             title: 'Swipe down to clear the card',
-                            hardm: 'Which year did World War I end?',
-                            answer: "1918", //answer
-                            choices: ["1944", "1918", "1914", "1980"],
+                            hardm: 'What is the probability of drawing a heart from a deck of cards?',
+                            answer: "0.25", //answer
+                            choices: ["0.40", "0.25", "0.20", "1"],
                             rank: 0.5 - Math.random()
                             }, {
                             title: 'Swipe down to clear the card',
-                            hardm: 'Which city is the capital of Canada?',
-                            answer: "Ottawa", //answer
-                            choices: ["Vancouver", "Toronto", "Ottawa", "Montreal"],
+                            hardm: '8 x 8 + 8 - 8 - 8 x 8 = ?',
+                            answer: "0", //answer
+                            choices: ["0", "1", "8", "64"],
                             rank: 0.5 - Math.random()
                             }, {
                             title: 'Swipe down to clear the card',
-                            hardm: 'In Canada, which national holiday occurs on July 1st?',
-                            answer: "Canada Day", //answer
-                            choices: ["Family Day", "Victory Day", "Victoria Day", "Canada Day"],
+                            hardm: '1 x 1 + 1 - 1 + 1 x 1 + 1 = ?',
+                            answer: "3", //answer
+                            choices: ["1", "3", "2", "0"],
                             rank: 0.5 - Math.random()
                             }, {
                             title: 'Swipe down to clear the card',
-                            hardm: 'Which city is the capital of BC?',
-                            answer: "Victoria", //answer
-                            choices: ["Victoria", "Vancouver", "Ottawa", "Edmonton"],
+                            hardm: '5 + 5 x 5 + 5 - 5?',
+                            answer: "30", //answer
+                            choices: ["10", "20", "30", "25"],
                             rank: 0.5 - Math.random()
                             }
                             ];
