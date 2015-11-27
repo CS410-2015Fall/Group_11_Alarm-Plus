f639497a4d6b86f7fdb0d68fce2450a00a384bdd
Aor_N <scc385@gmail.com>
2015-11-27 00:03:46 -0800

    SetUp Success

--- controllerTest.js
+++ controllerTest.js
@@ -3,6 +3,6 @@ http.createServer(function(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    res.end('Hello Travis!\n') // this will FAIL travis ci lint
+    res.end('Hello Travis!\n'); // this will FAIL travis ci lint
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
