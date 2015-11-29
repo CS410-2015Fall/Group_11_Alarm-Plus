b3f84f1525b24d8c31e8ecaeb73afc9f099157ab
instadelete <hyh912@gmail.com>
2015-11-28 23:39:00 -0800

    latest master with snake updated stuff

---
+++ tests/snaketest.js
@@ -0,0 +1,11 @@
+describe("length met", function() {
+	it("sets the goal of score to 3 fruits", function() {
+		expect(lengthmet()).toEqual(3);
+	});
+});
+
+describe("length met", function() {
+	it("sets the goal of score to 3 fruits", function() {
+		expect(lengthmet()).toEqual(4);
+	});
+});
