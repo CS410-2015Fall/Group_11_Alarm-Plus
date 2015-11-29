b234a96466580930f72916de6d332f0b4c6b4564
instadelete <hyh912@gmail.com>
2015-11-28 23:00:48 -0800

    including a failing test to see if the jasmine test is written correct for travis to read. if build fails its working

--- tests/tasktests/snaketest.js
+++ tests/tasktests/snaketest.js
@@ -3,3 +3,9 @@ describe("length met", function() {
 		expect(lengthmet()).toEqual(3);
 	});
 });
+
+describe("length met", function() {
+	it("sets the goal of score to 3 fruits", function() {
+		expect(lengthmet()).toEqual(4);
+	});
+});
